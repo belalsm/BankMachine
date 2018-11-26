2a6286fbde5c0a7a2654e85533e2e5b2b93728d2
belalsm <belalsm95@gmail.com>
2018-11-26 13:59:26 -0500

    Add other screens

---
+++ BankMachine/AccountBalances.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BankMachine
+{
+    /// <summary>
+    /// Interaction logic for AccountBalances.xaml
+    /// </summary>
+    public partial class AccountBalances : UserControl
+    {
+        public AccountBalances()
+        {
+            InitializeComponent();
+        }
+    }
+}
